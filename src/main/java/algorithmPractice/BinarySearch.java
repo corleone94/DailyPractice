8fca70d1c4070aa4d972e50f3ed220d56bc9cc26
王浩臣 <haochen.wang@tadu.com>
2021-11-18 00:59:05 +0800

    递归二分

--- src/main/java/algorithmPractice/BinarySearch.java
+++ src/main/java/algorithmPractice/BinarySearch.java
@@ -6,7 +6,9 @@ public class BinarySearch {
         int[] arr = new int[]{1,2,4,6,7};
         int target = 6;
         int index = binarySearch(arr, target);
-        System.out.println(index == -1 ? "没找到" : "index: " + index);
+        System.out.println("while："  + (index == -1 ? "没找到" : "index: " + index));
+        int index2 = binarySearchRecursion(arr, 0, arr.length-1, target);
+        System.out.println("递归："  + (index2 == -1 ? "没找到" : "index: " + index2));
     }
 
     private static int binarySearch(int[] arr, int target) {
@@ -29,4 +31,19 @@ public class BinarySearch {
 
         return -1;
     }
+
+    private static int binarySearchRecursion(int[] arr, int start, int end, int target) {
+        int middleIndex = (end + 1 + start)/2;
+        int middleNum = arr[middleIndex];
+        if (middleNum == target) {
+            return middleIndex;
+        }
+        if (middleNum > target) {
+            return binarySearchRecursion(arr, start, middleIndex, target);
+        } else {
+            return binarySearchRecursion(arr, middleIndex, end, target);
+        }
+
+    }
+
 }
